2379b1f3bec23e4dc41d9d38ad0dc24949475926
Krishna Tandon <tandonkrishna1208@gmail.com>
2025-05-18 17:38:35 +0530

    Github workflow updates

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="matholib",
+    version="0.1.0",
+    packages=find_packages(include=["matholib", "matholib.*"]),
+    install_requires=[],
+)
